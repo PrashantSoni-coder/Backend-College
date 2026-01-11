6b0e7c83fc03b310c643c4a797c9fa21c094f539
PrashantSoni-coder <soniprashant562005@gmail.com>
2026-01-11 19:37:22 +0530

    learned named and default exports

---
+++ lec2(typesOfExport)/first.js
@@ -0,0 +1,13 @@
+// function infor(){
+//     console.log("printMsg");
+// }
+// function info(){
+//     console.log("Prashant");
+// }
+// exports.add=()=>{
+//     console.log("add called");
+// }
+function add2(){
+    console.log("add2");
+} 
+ module.exports={addi:add2};
---
+++ lec2(typesOfExport)/second.js
@@ -0,0 +1,7 @@
+// const {infor,info,} = require("./first");
+// const data = require("./first");
+const data = require("./first");
+// infor();
+// info();
+// data.add();
+data.addi();
