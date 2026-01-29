b5f5717d235aedfbbd534c00a474ee2edcd0a056
Prashant Soni <prashantsoni@Prashants-MacBook-Air.local>
2026-01-29 13:36:42 +0530

    url encoding

--- lec6(HTTP)/index.js
+++ lec6(HTTP)/index.js
@@ -1,13 +1,2 @@
-const http = require("http");
-let server  = http.createServer((request,response)=>{
-    console.log(request);
-    response.end("Server started sucessfully");
-});
-server.listen(8008,'localhost',(err)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log("Server started at http://localhost:8008");
-});
 
 //next topics-> headers,url,status codes
