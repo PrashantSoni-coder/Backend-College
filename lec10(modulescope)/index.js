2066b4e5d6ef8dc4902d9ba8609808a424e405d9
Prashant Soni <prashantsoni@Prashants-MacBook-Air.local>
2026-02-07 01:46:48 +0530

    module scope

---
+++ lec10(modulescope)/index.js
@@ -0,0 +1,14 @@
+// require("./m1");
+// require("./m2");
+
+
+// module wraper function 
+(function (){
+    let name = "harry";
+    console.log(name);
+})();
+
+(function (){
+    let name = "garry";
+    console.log(name);
+})();
